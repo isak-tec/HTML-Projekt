e1cd8de5bda07eb17b251a2aa31b5ccc17ab999d
Reso <lulu9@elev.tec.dk>
2026-04-10 13:10:23 +0200

    Css and html changes
    Changes Css style, html changes  and data file py

---
+++ Data/B_page_db.py
@@ -0,0 +1,24 @@
+class Database():
+    def __init__(self):
+        self.counter = 0
+        self.current_user = None
+        self.users = {"admin":"pass",
+                      "Isak":"Elev",
+                      "Odin":"Elev1",
+                      "Lucian":"Hej-nej-new-now"}
+        self.naviList = [
+            "posts",
+            "test1",
+            "test2",
+            "test3",
+            "test4",
+            "test5",
+            "test6",
+            "test7",
+            "test8",
+            "test9",
+            "test10",
+            "test12",
+            "test13",
+        ]
+       
--- Main.py
+++ Main.py
@@ -1,2 +1,96 @@
-Hello isak
-Hello Isak
+#Hello isak
+#Hello Isak
+
+from sanic import Sanic
+from sanic.response import redirect
+from sanic_jinja2 import SanicJinja2
+from Data.B_page_db import Database
+
+app = Sanic("Website_example")
+
+# Serve static files
+app.static("/static", "./static")
+
+# Initialize Jinja2
+jinja = SanicJinja2(app)
+
+@app.before_server_start
+async def attach_db(app):
+    #This a database for the servers globals, NOT SECURE METHOD
+    app.ctx.db = Database()
+
+    # Initialize
+    if not hasattr(app.ctx,"counter"):
+        app.ctx.counter = 0
+
+    if  not hasattr(app.ctx,"current_user"):
+        app.ctx.current_user = None
+
+@app.get("/")
+@jinja.template("Html.html")
+async def frontpage(request):
+                                #TODO add a key "counter" and a value of the apps counter to the return statements dictionary
+                                #TODO current_user should not be hardcoded, this todo demands that later login task is done
+   
+    return {"naviList":app.ctx.db.naviList,"current_user":app.ctx.db.current_user, "counter":app.ctx.db.counter}
+
+#TODO: add /about endpoint, remember that you need to create a new html file to handle this
+#TODO: advanced task is to create a header.html and 
+#include the header via templating instead of copying to every new file
+
+
+@app.post("/addition")
+async def addition(request):
+    #TODO increment the value of the apps counter
+    app.ctx.db.counter +=1
+    return redirect("/")
+
+@app.post("/subtraction")
+async def subtraction(request):
+#TODO: Add /subtraction endpoint and make it subtract counter by 1
+    app.ctx.db.counter -=1
+    return redirect("/")
+
+@app.post("/reset")
+async def reset(request):
+    app.ctx.db.counter = 0
+    return redirect("/")
+
+
+#TODO: Add /resetcounter endpoint, remember to add html form
+
+
+@app.post("/loginattempt")
+async def loginattempt(request):
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    if not username in app.ctx.db.users:
+        app.ctx.db.current_user = username
+        return redirect("/")
+    
+    elif not password == app.ctx.db.users[username]:
+        app.ctx.db.current_user = None
+        return redirect("/")
+
+    else:
+        app.ctx.db.current_user = username
+        return redirect("/")
+    
+    #TODO: insert logic here, to confirm login and set a state to current_user
+    #TODO: Advanced task is to try using sanic cookies
+
+
+
+@app.post("/logout")
+async def logout(request):
+    #TODO: use this endpoint to logout of the app
+    #TODO: Advanced task is to logout by deleting a cookie
+    return redirect("/")
+
+if __name__ == "__main__":
+    app.run(host="localhost", port=8080)
+
+#TODO: Advanced task, read about CRUD patterns and implement CRUD for users in the app
+#TODO: Even more advanced task, read about how hashing of passwords works, and learn to use bcrypt library
+#TODO: If motivation still exists, learn to store the users in a .json file and use cookies
